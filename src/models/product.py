266c97b04fd686f05c654158b4f5828132582173
Alceu Ramos <alceu.rcjr@gmail.com>
2025-02-01 18:31:55 -0300

    feat: Define Product model

---
+++ src/models/product.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, field_validator
+
+
+class Category(Enum):
+    meal = "meal"
+    side_dish = "side-dish"
+    beverage = "beverage"
+    dessert = "dessert"
+
+
+class Product(BaseModel):
+    id: int | None = None
+    name: str
+    category: str
+    price: int
+    description: str
+    image: str
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+    @field_validator("category")
+    @classmethod
+    def validate_category(cls, v: str):
+        try:
+            Category(v)
+            return v
+        except ValueError:
+            raise ValueError(f"Invalid category value: {v}")
