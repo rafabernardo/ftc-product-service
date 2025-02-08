8eb37aa0ca4fa4e675691e2375dabe72a9a639ab
Rafaela Rodrigues <rafaela@highnote.io>
2025-02-08 20:12:55 -0300

    test: Consolidate mock environment setup for improved clarity and consistency

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,29 +8,6 @@ from dependency_injector import providers
 from src.core.dependency_injection import Container
 
 
-@pytest.fixture
-def mock_mongo_collection():
-    collection_mock = Mock()
-    return collection_mock
-
-
-def mocked_database():
-    client_mock = mongomock.MongoClient()
-    return client_mock.db
-
-
-@pytest.fixture
-def container():
-    return Container()
-
-
-@pytest.fixture
-def mock_mongo_db(container: Container):
-    container.mongo_database.override(providers.Factory(mocked_database))
-    yield
-    container.mongo_database.reset_override()
-
-
 @pytest.fixture(autouse=True)
 def mock_env():
     with patch.dict(
@@ -45,3 +22,26 @@ def mock_env():
         },
     ):
         yield
+
+
+@pytest.fixture
+def mock_mongo_collection():
+    collection_mock = Mock()
+    return collection_mock
+
+
+def mocked_database():
+    client_mock = mongomock.MongoClient()
+    return client_mock.db
+
+
+@pytest.fixture
+def container(mock_env):
+    return Container()
+
+
+@pytest.fixture
+def mock_mongo_db(container: Container):
+    container.mongo_database.override(providers.Factory(mocked_database))
+    yield
+    container.mongo_database.reset_override()
