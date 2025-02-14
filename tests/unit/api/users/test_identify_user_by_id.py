92f43b54768144d98180ccbaa049a0b9325b1a02
Rafaela Rodrigues <rafaela@highnote.io>
2025-02-14 19:53:05 -0300

    test: Add unit tests for user identification with various error scenarios

--- tests/unit/api/users/test_identify_user_by_id.py
+++ tests/unit/api/users/test_identify_user_by_id.py
@@ -3,8 +3,18 @@ from unittest.mock import Mock
 import pytest
 from fastapi.testclient import TestClient
 
+from api.v1.exceptions.commons import (
+    InternalServerErrorHTTPException,
+    NoDocumentsFoundHTTPException,
+    UnprocessableEntityErrorHTTPException,
+)
 from api.v1.users import router
 from core.dependency_injection import Container
+from core.exceptions.commons_exceptions import NoDocumentsFoundException
+from core.exceptions.user_exceptions import (
+    UserAlreadyExistsError,
+    UserInvalidFormatDataError,
+)
 from services.user_service import UserService
 
 client = TestClient(router)
@@ -39,3 +49,67 @@ def test_get_user_by_id_success(user_service_mock, user_mock):
     response = client.patch(f"users/identify/{user_id}", json=request_data)
 
     assert response.status_code == 200
+
+
+def test_identify_user_no_documents_found(user_service_mock):
+    user_id = "123"
+    request_data = {"cpf": "12345678900"}
+
+    user_service_mock.identify_user.side_effect = NoDocumentsFoundException()
+    with pytest.raises(
+        NoDocumentsFoundHTTPException, match="No document found"
+    ):
+
+        response = client.patch(f"users/identify/{user_id}", json=request_data)
+
+        assert response.status_code == 404
+
+
+def test_identify_user_invalid_format(user_service_mock):
+    user_id = "123"
+    request_data = {"cpf": "invalid_cpf"}
+
+    user_service_mock.identify_user.side_effect = UserInvalidFormatDataError(
+        "Invalid CPF format"
+    )
+    with pytest.raises(
+        UnprocessableEntityErrorHTTPException, match="Invalid CPF format"
+    ):
+
+        response = client.patch(f"users/identify/{user_id}", json=request_data)
+
+        assert response.status_code == 422
+        assert response.json()["detail"] == "Invalid CPF format"
+
+
+def test_identify_user_already_exists(user_service_mock):
+    user_id = "123"
+    request_data = {"cpf": "12345678900"}
+
+    user_service_mock.identify_user.side_effect = UserAlreadyExistsError(
+        "User already has a CPF"
+    )
+
+    with pytest.raises(
+        UnprocessableEntityErrorHTTPException, match="User already has a CPF"
+    ):
+        response = client.patch(f"users/identify/{user_id}", json=request_data)
+
+        assert response.status_code == 422
+        assert response.json()["detail"] == "User already has a CPF"
+
+
+def test_identify_user_internal_server_error(user_service_mock):
+    user_id = "123"
+    request_data = {"cpf": "12345678900"}
+
+    user_service_mock.identify_user.side_effect = Exception(
+        "Internal server error"
+    )
+
+    with pytest.raises(
+        InternalServerErrorHTTPException, match="Internal server error"
+    ):
+        response = client.patch(f"users/identify/{user_id}", json=request_data)
+
+        assert response.status_code == 500
