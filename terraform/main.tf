44e1930d58cadfa8c146ce8d945f81d49aa28bc9
Rafaela Rodrigues <rafaela@highnote.io>
2025-02-16 17:25:18 -0300

    feat: Add ECR login step to CI workflow for main branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
-module "ecr" {
-  source = "./modules/ecr"
+resource "aws_ecr_repository" "ecr_product_service" {
+  name                 = "ecr_product_service"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
 }
