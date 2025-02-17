6ba141fbde28435cf3764c8a7c15aac548578922
Rafaela Rodrigues <rafaela@highnote.io>
2025-02-16 22:38:15 -0300

    feat: Update token validation endpoint and improve error handling in authentication

--- src/external/authentication.py
+++ src/external/authentication.py
@@ -9,7 +9,7 @@ settings = get_settings()
 async def validate_token(token: str) -> dict:
     # URL of the token validation endpoint in the other microservice
     validate_token_url = (
-        f"http://{settings.AUTHENTICATION_URL}/service/v1/login"
+        f"http://{settings.AUTHENTICATION_URL}/service/v1/validate-token"
     )
     print(validate_token_url)
     # Make a request to the other microservice
@@ -19,12 +19,14 @@ async def validate_token(token: str) -> dict:
                 validate_token_url,
                 headers={"Authorization": f"Bearer {token}"},
             )
-            return response.json()  # Return the validated token payload
-        except HTTPException as e:
+            if response.status_code == 200:
+                return response.json()  # Return the validated token payload
+
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Invalid token",
-            ) from e
+            )
+
         except Exception as e:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
